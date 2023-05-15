e41789b61605152ea020f51b9edfd0c3699e9bca
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-15 13:53:14 -0500

    resize images in pop-up, drop in better backgrnd

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -8,10 +8,11 @@ export function PostsIndex(props) {
           <div key={post.id} className="col-sm-6 mb-3 mb-sm-3">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">{post.title}</h5>
+                <h2 className="card-title">{post.title}</h2>
                 <p className="card-text">{post.body}</p>
                 <img src={post.image || "https://t3.ftcdn.net/jpg/02/62/89/56/360_F_262895694_ZTEujfCykcG2vJoA0UVgZIPK1dTMlc9B.jpg"} />
-                <p><button className="btn btn-primary" onClick={() => {props.onShowPost(post)}}>More info</button></p>
+                
+                <p><button className="btn btn-warning mt-3 text-center" onClick={() => {props.onShowPost(post)}}>More info</button></p>
               </div>
             </div>
           </div>
