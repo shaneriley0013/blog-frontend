03c89e2e874b1c8158448b4ee20806bc1dbbca13
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-16 13:04:05 -0500

    make login/signup to match guide

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,22 +1,34 @@
-import axios from "axios"
+import axios from "axios";
+import { useState } from "react";
 
 export function Signup() {
+  const [errors, setErrors] = useState([]);
+
   const handleSubmit = (event) => {
-    console.log(event);
-    event.preventDefault()
-    const params = new FormData(event.target)
-    axios.post("http://localhost:3000/users.json", params).then(response => {
-      console.log(response.data);
-      event.target.reset();
-    }).catch(error => {
-      console.log(error.response.data)
-    })
-    console.log('handling submit');
-  }
-  
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/users.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+        setErrors(error.response.data.errors);
+      });
+  };
+
   return (
     <div id="signup">
       <h1>Signup</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
       <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
